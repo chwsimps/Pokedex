faaa96e428772ce93d578bfd0251ed86a8ec8dc1
Chase Simpson <chase.w.simpson1@gmail.com>
2023-06-19 18:09:47 -0400

    Added eslint and prettier for formatting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,40 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'eslint-config-prettier',
   ],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    warnOnUnsupportedTypeScriptVersion: false,
+  },
   plugins: ['react-refresh'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
   rules: {
-    'react-refresh/only-export-components': 'warn',
+    'no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'after-used',
+        ignoreRestSiblings: true,
+        argsIgnorePattern: '^_',
+      },
+    ],
+    quotes: ['error', 'single'],
+    'no-duplicate-imports': 'error',
+    'react/react-in-jsx-scope': 'off',
   },
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
-
 function App() {
   return (
     <>
       <h1>Pokedex</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
